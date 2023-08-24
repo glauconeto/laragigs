60809f5be5629b389141a3a149a3d2fc580f06c1
glauconeto <glauco.neto@outlook.com>
2023-08-24 19:20:50 -0300

    routes, api routes, database seed and migrations

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// All Listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Lastest Listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+// Single Listing
+Route::get('/listings/{id}', function($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
+});
+
+Route::get('/hello', function () {
+    return response('Hello world', 200)
+            ->header('Content-Type', 'text/plain')
+            ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    ddd($id);
+    return response('Post ', $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request) {
+    return$request->name . ', ' . $request->city;
 });
