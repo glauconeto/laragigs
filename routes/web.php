1ab200aa4693db05e9877d2e3ba0b2723b567c38
glauconeto <glauco.neto@outlook.com>
2023-09-30 14:34:14 -0300

    middlewares de usuários e visitantes.

--- routes/web.php
+++ routes/web.php
@@ -45,34 +45,34 @@ use App\Http\Controllers\ListingController;
 Route::get('/', [ListingController::class, 'index']);
 
 // Mostra formulário de criação.
-Route::get('/listings/create', [ListingController::class, 'create']);
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
 
 // Armazena dados do formulário.
-Route::post('/listings', [ListingController::class, 'store']);
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
 
 // Mostra formulário de edição.
-Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
 
 // Atualiza dados.
-Route::put('/listings/{listing}', [ListingController::class, 'update']);
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
 
 // Remove item.
-Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
 
 // Mostra apenas um item
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 // Mostra o formulário de registro de usuários.
-Route::get('/register', [UserController::class, 'create']);
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
 
 // Cria novo usuário.
 Route::post('/users', [UserController::class, 'store']);
 
 // Desloga o usuário.
-Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
 
 // Mostra formulário de login.
-Route::get('/login', [UserController::class, 'login']);
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
 
 // Loga o usuário no sistema.
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
